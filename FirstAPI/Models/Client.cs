dc38ac7a901f395593cb9735fafc06e91cd6395e
Rafael <devrafalcoelho@gmail.com>
2023-05-21 17:12:27 -0300

    client model and viewmodel

---
+++ FirstAPI/Models/Client.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.Models;
+
+public class Client
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O Nome é obrigatório.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "O E-mail é obrigatório.")]
+    [EmailAddress(ErrorMessage = "E-mail inválido.")]
+    public string? Email { get; set; }
+
+
+    [Required(ErrorMessage = "O CPF é obrigatório.")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "CPF inválido.")]
+    public string? CPF { get; set; }
+
+
+    [Required(ErrorMessage = "O Número de Telefone é obrigatório.")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "Número de Telefone inválido.")]
+    public string? Phone { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Complement { get; set; }
+
+    [StringLength(8, MinimumLength = 8, ErrorMessage = "CEP inválido.")]
+    public string? Zip_Code { get; set; }
+
+    public string? District { get; set; }
+
+    public string? City { get; set; }
+
+    [StringLength(2, MinimumLength = 2, ErrorMessage = "Sigla de Estado inválida.")]
+    public string? UF { get; set; }
+}
+
---
+++ FirstAPI/ViewModels/ClientViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.ViewModels;
+
+public class ClientViewModel
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? CPF { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Complement { get; set; }
+
+    public string? Zip_Code { get; set; }
+
+    public string? District { get; set; }
+
+    public string? City { get; set; }
+
+    public string? UF { get; set; }
+}
+
